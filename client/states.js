771ad0a08e4dd3e3b59362d88379897eba84e957
Yulong Hu <Yulong.Hu@multek.com>
2017-10-23 01:25:30 -0700

    [Studio] Add screen "DemoModel"

--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.demomodel', {
+        views: {
+            app: {
+                controller: 'app_demomodel',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.demomodel');
+                }
+            }
+        }
     });
 });
