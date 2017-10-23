9ffa5266102a7c9b5a53c9185c045687d79dcbf6
Yulong Hu <Yulong.Hu@multek.com>
2017-10-23 01:32:15 -0700

    [Studio] Add screen "namelist"

--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.namelist', {
+        views: {
+            app: {
+                controller: 'app_namelist',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.namelist');
+                }
+            }
+        }
     });
 });
