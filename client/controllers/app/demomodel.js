fa0dc5280d1d5179e18486e453eb36d24dc421a9
Yulong Hu <Yulong.Hu@multek.com>
2017-10-23 01:30:32 -0700

    [Studio] Delete screen "app.demomodel"

--- client/controllers/app/demomodel.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('app_demomodel', app_demomodel);
-function app_demomodel($scope, app) {
-    'use strict';
-    app.init($scope);
-}
--- client/states.js
+++ client/states.js
@@ -51,14 +51,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.demomodel', {
-        views: {
-            app: {
-                controller: 'app_demomodel',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.demomodel');
-                }
-            }
-        }
     });
 });
