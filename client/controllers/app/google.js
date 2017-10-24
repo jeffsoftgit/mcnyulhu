a5efc7334b72a66a8814d8e4b3678dcb9da82d0a
Yulong Hu <Yulong.Hu@multek.com>
2017-10-23 21:29:12 -0700

    [Extension] Create screen "google"

---
+++ client/controllers/app/google.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_google', app_google);
+function app_google($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -69,5 +69,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.google', {
+        views: {
+            app: {
+                controller: 'app_google',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.google');
+                }
+            }
+        }
     });
 });
