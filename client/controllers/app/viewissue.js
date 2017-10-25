e453acd728fe4956efb78f0cd1cf3fbacf994ff2
Yulong Hu <Yulong.Hu@multek.com>
2017-10-24 19:50:31 -0700

    [Studio] Modified screen "viewissue"

--- client/controllers/app/viewissue.js
+++ client/controllers/app/viewissue.js
@@ -3,6 +3,10 @@ function app_viewissue($scope, app) {
     'use strict';
     app.init($scope)
     {
-        alert('hello');
+        var data=$scope.data;
+        for(var i=0;i<data.issuelist.length;i++)
+        {
+            data.issuelist[i].summary='this me';
+        }
     }
 }
