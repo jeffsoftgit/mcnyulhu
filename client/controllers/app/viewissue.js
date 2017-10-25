41d7fb749da78ba06f530484a3aff20421ad662d
Yulong Hu <Yulong.Hu@multek.com>
2017-10-24 19:48:19 -0700

    [Studio] Modified screen "viewissue"

--- client/controllers/app/viewissue.js
+++ client/controllers/app/viewissue.js
@@ -1,5 +1,8 @@
 angular.module('app').controller('app_viewissue', app_viewissue);
 function app_viewissue($scope, app) {
     'use strict';
-    app.init($scope);
+    app.init($scope)
+    {
+        alert('hello');
+    }
 }
